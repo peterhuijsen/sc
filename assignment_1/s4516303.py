a983999a5fed0c67541dffb6d58cecf1197272be
Marlon Fendler <marlon.fendler24@gmail.com>
2026-02-25 14:57:54 +0100

    chore

--- assignment_1/s4516303.py
+++ assignment_1/s4516303.py
@@ -1,22 +1,26 @@
 import sys
 import string
 from hashlib import sha256
+from typing import Callable, Any
 
 level, input_file = int(sys.argv[1]), sys.argv[2]
 with open(input_file, "r") as f:
     lines = [l.strip().split(",") for l in f.readlines()]
 
-def calculate_combinations(characters, previous_combinations):
-    # We append each character for each previous combination.
+def calculate_combinations(characters: list[str], previous_combinations: list[str], depth_remaining: int = 1) -> list[str]:
     return [previous + character for previous in previous_combinations for character in characters] \
         if len(previous_combinations) > 0 \
         else characters
     
-def simple_crack_hashes(hashes, characters, lengths, check_func):
-    found = []
+
+def simple_crack_hashes(hashes: list[Any], characters: list[str], lengths: tuple[int, int], check_func: Callable[[list[Any], str], list[tuple[str, str]]]) -> list[tuple[str, str]]:
+    found: list[tuple[str, str]] = []
     combinations = []
+
+    # check whether the last generated combinations already hit the maximum length of the passwords
     while len(combinations[-1] if len(combinations) > 0 else []) < lengths[1]:
         combinations = calculate_combinations(characters, combinations)
+
         for combination in combinations:
             # Skip any combinations which aren't checked.
             if len(combination) < lengths[0]:
@@ -26,11 +30,12 @@ def simple_crack_hashes(hashes, characters, lengths, check_func):
             
     return found
 
-def simple_check_function(hashes, combination):
+def simple_check_function(hashes: list[str], combination: str) -> list[tuple[str, str]]:
     attempt_hash = sha256(combination.encode("utf-8")).hexdigest()
     return [(h, combination) for h in hashes if h == attempt_hash]
 
-def salted_check_function(hashes, combination):
+
+def salted_check_function(hashes: list[tuple[str, str]], combination: str) -> list[tuple[str, str]]:
     found = []
     for hash, salt in hashes:
         attempt_hash = sha256(combination.encode("utf-8") + salt.encode("utf-8")).hexdigest()
@@ -39,7 +44,8 @@ def salted_check_function(hashes, combination):
 
     return found
 
-def mutation_check_function(hashes, combination):
+
+def mutation_check_function(hashes: list[tuple[str, str]], combination: str) -> list[tuple[str, str]]:
     found = []
     for hash, salt in hashes:
         attempt_hash = sha256(combination.encode("utf-8") + salt.encode("utf-8")).hexdigest()
@@ -48,7 +54,8 @@ def mutation_check_function(hashes, combination):
 
     return found
 
-def level_1(csv):
+
+def level_1(csv: list[list[str]]) -> None:
     characters = [str(c) for c in range(10)]
     hashes = [row[1] for row in csv[1:]]
     matches = simple_crack_hashes(
@@ -58,11 +65,13 @@ def level_1(csv):
         check_func=simple_check_function 
     )
     
+    # works but prolly slow for large csvs
     for row in csv[1:]:
         match = next(iter([m[1] for m in matches if m[0] == row[1]]), None)
         print(f"{row[0]}, {match}")
         
-def level_2(csv):
+        
+def level_2(csv: list[list[str]]) -> None:
     characters = list[str](string.ascii_lowercase)
     hashes = [row[1] for row in csv[1:]]
     matches = simple_crack_hashes(
@@ -76,7 +85,7 @@ def level_2(csv):
         match = next(iter([m[1] for m in matches if m[0] == row[1]]), None)
         print(f"{row[0]}, {match}")
 
-def level_3(csv):
+def level_3(csv: list[list[str]]) -> None:
     characters = list[str](string.ascii_lowercase)
     hash_salts = [(row[2], row[1]) for row in csv[1:]]
     matches = simple_crack_hashes(
@@ -90,7 +99,7 @@ def level_3(csv):
         match = next(iter([m[1] for m in matches if m[0] == row[2]]), None)
         print(f"{row[0]}, {match}")
 
-def level_4(csv):
+def level_4(csv: list[list[str]]) -> None:
     characters = ["Margrave", "Whalebones", "Muskroots", "Atmometry", "Fourdriniers", "Porrecting", "Pericynthions", "Misfeasors", "Histiology", "Parochin", "Knockwurst", "Rubeolar", "Ensampling", "Plugless", "Cornerback", "Grapplers", "Coronachs", "Waughted", "Catarhine", "Skidpans", "Fleshmongers", "Provenance", "Trollied", "Selaginellas", "Creepages", "Humectated", "Cercopids", "Unproclaimed", "Bourtrees", "Thalamic", "Inerasably", "Panspermatist", "Reheatings", "Changeless", "Karabiners", "Nonexempt", "Hypoplasties", "Shirtier", "Stablenesses", "Eavesdrop", "Recommittal", "Parrotier", "Obtemperates", "Preprimaries", "Freebooting", "Biggetiest", "Reaffixing", "Alcoholisms", "Cartwheels", "Underreported"]
     hashes = [row[1] for row in csv[1:]]
     matches = simple_crack_hashes(
@@ -104,7 +113,7 @@ def level_4(csv):
         match = next(iter([m[1] for m in matches if m[0] == row[1]]), None)
         print(f"{row[0]}, {match}")
 
-def level_5(csv):
+def level_5(csv: list[list[str]]) -> None:
     mutations = [*[str(c) for c in range(10)], *[string.ascii_letters]]
     characters = list[str](string.ascii_lowercase)
     hashes = [row[1] for row in csv[1:]]
@@ -129,7 +138,7 @@ def level_5(csv):
 
         print(f"{row[0]}, {result}")
         
-def solve_level(level, csv):
+def solve_level(level: int, csv: list[list[str]]) -> None:
     match level:
         case 1:
             return level_1(csv)

